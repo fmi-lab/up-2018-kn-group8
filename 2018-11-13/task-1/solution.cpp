40664b14be4a7dcf5d304595b84b39b89d285a1e
Georgy Angelov <georgyangelov@gmail.com>
2018-11-13 11:42:22 +0200

    Remove tasks we didn't get to

--- 2018-11-13/task-1/solution.cpp
+++ 2018-11-13/task-1/solution.cpp
@@ -1,6 +1,18 @@
 // Изтрийте този ред, ако използвате Visual Studio
 #include "tests.cpp"
 
+void swap(double array[], int i, int j) {
+    double temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+}
+
 void sort(double array[], int length) {
-    // Попълнете този код
+    for (int i = 0; i < length; i++) {
+        for (int j = i; j < length; j++) {
+            if (array[j] < array[i]) {
+                swap(array, i, j);
+            }
+        }
+    }
 }
